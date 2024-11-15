7f7239974eb922c00aa3752f65a41be5a13bf7f6
Olivier Guenette <olivierguenette@hotmail.com>
2024-11-15 12:27:00 -0500

    update day 1

--- 2023/day-01/src/main.rs
+++ 2023/day-01/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let part_one_result = part_one(INPUT_STR);
     let part_two_result = part_two(INPUT_STR);
 
-    println!("AOC Day 1");
+    println!("AOC {}", env!("CARGO_PKG_NAME"));
     println!(" ---- ");
     println!("Part 1: {:?}", part_one_result);
     println!("Part 2: {:?}", part_two_result);
@@ -12,95 +12,80 @@ fn main() {
 }
 
 fn part_one(input: &str) -> Option<u32> {
-    Some(
-        input
-            .split('\n')
-            .filter(|v| !v.is_empty())
-            .map(|l| {
-                let mut first = 0;
-                let mut last = 0;
-                for c in l.chars() {
+    let result = input
+        .split('\n')
+        .filter(|v| !v.is_empty())
+        .map(|l| {
+            let mut first = 0;
+            let mut last = 0;
+            for c in l.chars() {
+                if let Some(v) = c.to_digit(10) {
+                    first = v;
+                    break;
+                }
+            }
+            for c in l.chars().rev() {
+                if let Some(v) = c.to_digit(10) {
+                    last = v;
+                    break;
+                }
+            }
+            first * 10 + last
+        })
+        .sum();
+    Some(result)
+}
+
+const WORD_TO_NUMBER: [&'static str; 9] = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
+
+fn get_val_from_subl(sub_l: &str) -> Option<u32> {
+    for i in 0..WORD_TO_NUMBER.len() {
+        if let Some(_) = sub_l.find(WORD_TO_NUMBER.get(i).unwrap()) {
+            return Some((i as u32) + 1);
+        }
+    }
+    None
+}
+
+fn part_two(input: &str) -> Option<u32> {
+    let result = input
+        .split('\n')
+        .filter(|v| !v.is_empty())
+        .map(|l| {
+            let mut first: u32 = 0;
+            let mut last: u32 = 0;
+
+            for i in 0..l.len() {
+                if let Some(c) = l.chars().nth(i) {
                     if let Some(v) = c.to_digit(10) {
                         first = v;
                         break;
                     }
                 }
-                for c in l.chars().rev() {
+                let (sub_l, _) = l.split_at(i + 1);
+                get_val_from_subl(sub_l).map(|v| first = v);
+                if first != 0 {
+                    break;
+                }
+            }
+
+            for i in 0..l.len() {
+                if let Some(c) = l.chars().rev().nth(i) {
                     if let Some(v) = c.to_digit(10) {
                         last = v;
                         break;
                     }
                 }
-                first * 10 + last
-            })
-            .sum(),
-    )
-}
-
-#[rustfmt::skip]
-const WORD_TO_NUMBER: [(&'static str, u8); 9] = [
-    ("one",    1),
-    ("two",    2),
-    ("three",  3),
-    ("four",   4),
-    ("five",   5),
-    ("six",    6),
-    ("seven",  7),
-    ("eight",  8),
-    ("nine",   9),
-];
-
-fn part_two(input: &str) -> Option<u32> {
-    Some(
-        input
-            .split('\n')
-            .filter(|v| !v.is_empty())
-            .map(|l| {
-                let mut first: u32 = 0;
-                let mut last: u32 = 0;
-
-                for i in 0..l.len() {
-                    if let Some(c) = l.chars().nth(i) {
-                        if let Some(v) = c.to_digit(10) {
-                            first = v;
-                            break;
-                        }
-                    }
-                    let (sub_l, _) = l.split_at(i + 1);
-                    for (label, val) in WORD_TO_NUMBER {
-                        if let Some(_) = sub_l.find(label) {
-                            first = val as u32;
-                            break;
-                        }
-                    }
-
-                    if first != 0 {
-                        break;
-                    }
+                let (_, sub_l) = l.split_at(l.len() - i - 1);
+                get_val_from_subl(sub_l).map(|v| last = v);
+                if last != 0 {
+                    break;
                 }
-
-                for i in 0..l.len() {
-                    if let Some(c) = l.chars().rev().nth(i) {
-                        if let Some(v) = c.to_digit(10) {
-                            last = v;
-                            break;
-                        }
-                    }
-                    let (_, sub_l) = l.split_at(l.len() - i - 1);
-                    for (label, val) in WORD_TO_NUMBER {
-                        if let Some(_) = sub_l.find(label) {
-                            last = val as u32;
-                            break;
-                        }
-                    }
-
-                    if last != 0 {
-                        break;
-                    }
-                }
-                println!("{}: {} - {}", l, first, last);
-                first * 10 + last
-            })
-            .sum(),
-    )
+            }
+            first * 10 + last
+        })
+        .sum();
+    Some(result)
 }
