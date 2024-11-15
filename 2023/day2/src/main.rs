40b5261f17096e3cc478e596d7f265da528f14ef
Olivier Guenette <olivierguenette@hotmail.com>
2024-11-15 11:36:29 -0500

    add day 2

---
+++ 2023/day2/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    const INPUT_STR: &str = include_str!("../inputs/input.txt");
+
+    let part_one_result = part_one(INPUT_STR);
+    let part_two_result = part_two(INPUT_STR);
+
+    println!("AOC Day 2");
+    println!(" ---- ");
+    println!("Part 1: {:?}", part_one_result);
+    println!("Part 2: {:?}", part_two_result);
+    println!(" ---- ");
+}
+
+fn part_one(input: &str) -> Option<u32> {
+    None
+}
+
+fn part_two(input: &str) -> Option<u32> {
+    None
+}
