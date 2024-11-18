577b210e1ecf73050c6dae0347d31e6ff24d26f9
Olivier Guenette <olivierguenette@hotmail.com>
2024-11-18 17:50:59 -0500

    2023 day 2 part 2

--- 2023/day-02/src/main.rs
+++ 2023/day-02/src/main.rs
@@ -1,7 +1,6 @@
 const R_CUBES: u32 = 12;
 const G_CUBES: u32 = 13;
 const B_CUBES: u32 = 14;
-const T_CUBES: u32 = R_CUBES + G_CUBES + B_CUBES;
 
 fn main() {
     const INPUT_STR: &str = include_str!("../inputs/input.txt");
@@ -16,26 +15,25 @@ fn main() {
     println!("AOC Day 2");
     println!("------");
     println!("Part 1 ({part_one_time}): {part_one_result:?}");
-    println!("Part 2 ({part_one_time}): {part_two_result:?}");
+    println!("Part 2 ({part_two_time}): {part_two_result:?}");
     println!("------");
 }
 
 fn part_one(input: &str) -> u32 {
     let mut sum = 0;
     for game in input.lines() {
-        println!("{}", game);
-        let chunk: Vec<&str> = game.split(':').collect();
+        // println!("{}", game);
+        let id_and_pulls: Vec<&str> = game.split(':').collect();
 
-        let id: u32 = chunk[0].split(' ').collect::<Vec<&str>>()[1]
+        let id: u32 = id_and_pulls[0].split(' ').collect::<Vec<&str>>()[1]
             .parse()
             .unwrap();
-        println!("{}", id);
+        // println!("{}", id);
 
         let mut game_is_possible = true;
 
-        for pull in chunk[1].split(';') {
-            println!("\t{}", pull);
-
+        for pull in id_and_pulls[1].split(';') {
+            // println!("\t{}", pull);
             let mut r: u32 = 0;
             let mut g: u32 = 0;
             let mut b: u32 = 0;
@@ -44,12 +42,14 @@ fn part_one(input: &str) -> u32 {
                 let color_parts: Vec<&str> = color.trim().split(' ').collect();
                 let cubes = color_parts[0].parse::<u32>().unwrap();
 
-                match color_parts[1] {
-                    "red" => r = cubes,
-                    "blue" => b = cubes,
-                    "green" => g = cubes,
+                let c: &mut u32 = match color_parts[1] {
+                    "red" => &mut r,
+                    "blue" => &mut b,
+                    "green" => &mut g,
                     _ => unreachable!(),
-                }
+                };
+
+                *c = cubes;
             }
 
             if r > R_CUBES || b > B_CUBES || g > G_CUBES {
@@ -67,5 +67,34 @@ fn part_one(input: &str) -> u32 {
 }
 
 fn part_two(input: &str) -> u32 {
-    0
+    let mut sum = 0;
+    for game in input.lines() {
+        let chunk: Vec<&str> = game.split(':').collect();
+
+        let mut r: u32 = 0;
+        let mut g: u32 = 0;
+        let mut b: u32 = 0;
+
+        for pull in chunk[1].split(';') {
+            for color in pull.split(',') {
+                let color_parts: Vec<&str> = color.trim().split(' ').collect();
+                let cubes = color_parts[0].parse::<u32>().unwrap();
+
+                let c: &mut u32 = match color_parts[1] {
+                    "red" => &mut r,
+                    "blue" => &mut b,
+                    "green" => &mut g,
+                    _ => unreachable!(),
+                };
+
+                if cubes > *c {
+                    *c = cubes;
+                }
+            }
+        }
+
+        sum += r * b * g;
+    }
+
+    sum
 }
