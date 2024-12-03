225a9f6b6bd20bdfa1322ec816661838461d35af
Olivier Guenette <olivier.guenette@marcottesystems.com>
2024-12-03 10:37:53 -0500

    done 2024 day-03

--- 2024/day-03/src/main.rs
+++ 2024/day-03/src/main.rs
@@ -5,9 +5,88 @@ fn main() {
 }
 
 fn part_one(input: &str) -> usize {
-    0
+    let current_slice = input.trim();
+
+    current_slice
+        .chars()
+        .enumerate()
+        .filter_map(|(i, c)| match c {
+            'm' => parse_mul(&current_slice[i..]),
+            _ => None,
+        })
+        .sum()
 }
 
 fn part_two(input: &str) -> usize {
-    0
+    let mut enable = true;
+
+    let current_slice = input.trim();
+
+    current_slice
+        .chars()
+        .enumerate()
+        .filter_map(|(i, c)| {
+            let sub = &current_slice[i..];
+            match c {
+                'd' => {
+                    if enable && sub.find("don't()") == Some(0) {
+                        enable = false;
+                    }
+
+                    if !enable && sub.find("do()") == Some(0) {
+                        enable = true;
+                    }
+                    None
+                }
+                'm' => {
+                    if !enable {
+                        None
+                    } else {
+                        parse_mul(sub)
+                    }
+                }
+                _ => None,
+            }
+        })
+        .sum()
+}
+
+const MUL_PAREN: &str = "mul(";
+const PAREN: &str = ")";
+const COMMA: u8 = b',';
+
+fn parse_mul(sub: &str) -> Option<usize> {
+    if sub.find(MUL_PAREN) != Some(0) {
+        return None;
+    }
+
+    let end_pos = match sub.find(PAREN) {
+        Some(val) => val,
+        None => return None,
+    };
+
+    if let Some(_) = sub[MUL_PAREN.len()..end_pos].find(MUL_PAREN) {
+        return None;
+    }
+
+    let comma_pos = match sub.as_bytes().iter().position(|b| *b == COMMA) {
+        Some(v) => v,
+        None => return None,
+    };
+
+    let num1: usize = match sub[MUL_PAREN.len()..comma_pos].parse() {
+        Ok(val) => val,
+        Err(_) => {
+            return None;
+        }
+    };
+
+    let num2: usize = match sub[comma_pos + 1..end_pos].parse() {
+        Ok(val) => val,
+        Err(_) => {
+            return None;
+        }
+    };
+
+    return Some(num1 * num2);
 }
