04dd16850a8c5aa2ed80f9d8e5f38f7c076b145e
Olivier Guenette <olivier.guenette@marcottesystems.com>
2024-12-04 14:42:53 -0500

    optimize 2024 day-04

--- 2024/day-04/src/main.rs
+++ 2024/day-04/src/main.rs
@@ -6,12 +6,24 @@ fn main() {
 }
 
 fn is_xmas(word: &[&char]) -> bool {
-    word == &[&'X', &'M', &'A', &'S']
+    word == [&'X', &'M', &'A', &'S']
 }
 
 fn part_one(input: &str) -> usize {
-    let data: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
+    let mut data_t: Vec<Vec<char>> = input
+        .lines()
+        .map(|l| {
+            let mut cline = vec!['.'; 4];
+            cline.append(&mut l.chars().collect());
+            cline.append(&mut vec!['.'; 4]);
+            cline
+        })
+        .collect();
+    let mut data = vec![vec!['.'; data_t[0].len()]; 4];
+    data.append(&mut data_t);
+    data.append(&mut vec![vec!['.'; data[0].len()]; 4]);
 
+    let data = data;
     let mut count = 0;
 
     for (j, row) in data.iter().enumerate() {
@@ -21,91 +33,81 @@ fn part_one(input: &str) -> usize {
             }
 
             // Check diag down right
-            let x = data[j..]
+            let x: Vec<&char> = data[j..]
                 .iter()
                 .take(4)
                 .enumerate()
                 .filter_map(|(k, l)| l.get(i + k))
-                .collect::<Vec<&char>>();
+                .collect();
             if is_xmas(x.as_slice()) {
                 count += 1;
             }
 
             // Check vertical forward
-            let x = row[i..].iter().take(4).collect::<Vec<&char>>();
+            let x: Vec<&char> = row[i..].iter().take(4).collect();
             if is_xmas(x.as_slice()) {
                 count += 1;
             }
 
             // Check diag down left
-            let x = data[j..]
+            let x: Vec<&char> = data[j..]
                 .iter()
                 .take(4)
                 .enumerate()
-                .filter_map(|(k, l)| if i >= 3 { l.get(i - k) } else { None })
-                .collect::<Vec<&char>>();
+                .filter_map(|(k, l)| l.get(i - k))
+                .collect();
             if is_xmas(x.as_slice()) {
                 count += 1;
             }
 
             // Check vertical backward
-            if i >= 3 {
-                let x = row.iter().skip(i - 3).take(4).rev().collect::<Vec<&char>>();
-                if is_xmas(x.as_slice()) {
-                    count += 1;
-                }
-            }
-
-            // Check horizontal downward
-            let x = data[j..]
-                .iter()
-                .take(4)
-                .filter_map(|l| l.get(i))
-                .collect::<Vec<&char>>();
+            let x: Vec<&char> = row.iter().skip(i - 3).take(4).rev().collect();
             if is_xmas(x.as_slice()) {
                 count += 1;
             }
 
-            if j >= 3 {
-                // Check horizontal upward
-                let x = data
-                    .iter()
-                    .skip(j - 3)
-                    .take(4)
-                    .filter_map(|l| l.get(i))
-                    .rev()
-                    .collect::<Vec<&char>>();
-                if is_xmas(x.as_slice()) {
-                    count += 1;
-                }
+            // Check horizontal downward
+            let x: Vec<&char> = data[j..].iter().take(4).filter_map(|l| l.get(i)).collect();
+            if is_xmas(x.as_slice()) {
+                count += 1;
+            }
 
-                // Check diag up right
-                let x = data
-                    .iter()
-                    .skip(j - 3)
-                    .take(4)
-                    .rev()
-                    .enumerate()
-                    .filter_map(|(k, l)| l.get(i + k))
-                    .collect::<Vec<&char>>();
-                if is_xmas(x.as_slice()) {
-                    count += 1;
-                }
+            // Check horizontal upward
+            let x: Vec<&char> = data
+                .iter()
+                .skip(j - 3)
+                .take(4)
+                .filter_map(|l| l.get(i))
+                .rev()
+                .collect();
+            if is_xmas(x.as_slice()) {
+                count += 1;
+            }
+
+            // Check diag up right
+            let x: Vec<&char> = data
+                .iter()
+                .skip(j - 3)
+                .take(4)
+                .rev()
+                .enumerate()
+                .filter_map(|(k, l)| l.get(i + k))
+                .collect();
+            if is_xmas(x.as_slice()) {
+                count += 1;
             }
 
             // Check diag up left
-            if j >= 3 && i >= 3 {
-                let x = data
-                    .iter()
-                    .skip(j - 3)
-                    .take(4)
-                    .rev()
-                    .enumerate()
-                    .filter_map(|(k, l)| l.get(i - k))
-                    .collect::<Vec<&char>>();
-                if is_xmas(x.as_slice()) {
-                    count += 1;
-                }
+            let x: Vec<&char> = data
+                .iter()
+                .skip(j - 3)
+                .take(4)
+                .rev()
+                .enumerate()
+                .filter_map(|(k, l)| l.get(i - k))
+                .collect();
+            if is_xmas(x.as_slice()) {
+                count += 1;
             }
         }
     }
@@ -114,40 +116,47 @@ fn part_one(input: &str) -> usize {
 }
 
 fn part_two(input: &str) -> usize {
-    let data: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
+    let mut data_t: Vec<Vec<char>> = input
+        .lines()
+        .map(|l| {
+            let mut cline = vec!['.'; 1];
+            cline.append(&mut l.chars().collect());
+            cline.push('.');
+            cline
+        })
+        .collect();
+    let mut data = vec![vec!['.'; data_t[0].len()]; 1];
+    data.append(&mut data_t);
+    data.push(vec!['.'; data[0].len()]);
+
+    let data = data;
 
     data.iter()
         .enumerate()
-        .take(data.len() - 1)
-        .skip(1)
         .flat_map(|(j, row)| {
             let data_ref = &data;
-            row.iter()
-                .enumerate()
-                .take(row.len() - 1)
-                .skip(1)
-                .filter_map(move |(i, &c)| {
-                    if c != 'A' {
-                        return None;
-                    }
+            row.iter().enumerate().filter_map(move |(i, &c)| {
+                if c != 'A' {
+                    return None;
+                }
 
-                    let bs = [
-                        data_ref.get(j - 1)?.get(i - 1)?,
-                        data_ref.get(j + 1)?.get(i + 1)?,
-                    ];
-                    let fs = [
-                        data_ref.get(j - 1)?.get(i + 1)?,
-                        data_ref.get(j + 1)?.get(i - 1)?,
-                    ];
+                let bs = [
+                    data_ref.get(j - 1)?.get(i - 1)?,
+                    data_ref.get(j + 1)?.get(i + 1)?,
+                ];
+                let fs = [
+                    data_ref.get(j - 1)?.get(i + 1)?,
+                    data_ref.get(j + 1)?.get(i - 1)?,
+                ];
 
-                    if ((bs == [&'M', &'S']) || (bs == [&'S', &'M']))
-                        && ((fs == [&'M', &'S']) || (fs == [&'S', &'M']))
-                    {
-                        return Some(());
-                    }
+                if ((bs == [&'M', &'S']) || (bs == [&'S', &'M']))
+                    && ((fs == [&'M', &'S']) || (fs == [&'S', &'M']))
+                {
+                    return Some(());
+                }
 
-                    None
-                })
+                None
+            })
         })
         .count()
 }
