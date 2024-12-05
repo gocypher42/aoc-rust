7866a154e36b04c6af1312923fcb542a2526ba93
Olivier Guenette <olivier.guenette@marcottesystems.com>
2024-12-05 10:48:38 -0500

    start 2024 day-06

---
+++ 2024/day-06/src/main.rs
@@ -0,0 +1,14 @@
+#[allow(clippy::wildcard_imports)]
+use utils::*;
+
+fn main() {
+    aoc_main!("../inputs/input.txt");
+}
+
+fn part_one(input: &str) -> usize {
+    0
+}
+
+fn part_two(input: &str) -> usize {
+    0
+}
